be5ec3d6aa3d5f4c716b2037acaaf4f31e8ae422
node-shubham <node.shubham@gmail.com>
2023-01-11 16:14:43 +0530

    code deletion

--- AMS/Debug/Core/Src/user_global.d
+++
@@ -1,3 +0,0 @@
-Core/Src/user_global.o: ../Core/Src/user_global.c \
- ../Core/Inc/user_global.h
-../Core/Inc/user_global.h:
--- AMS/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
+++
@@ -1,6 +0,0 @@
-ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
- ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
- E:/UNDER\ DEVELOPMENT/ATTENDANCE\ MONITORING\ SYSTEM/AMS/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- e:\under\ development\attendance\ monitoring\ system\ams\thirdparty\segger\config\segger_rtt_conf.h
-E:/UNDER\ DEVELOPMENT/ATTENDANCE\ MONITORING\ SYSTEM/AMS/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-e:\under\ development\attendance\ monitoring\ system\ams\thirdparty\segger\config\segger_rtt_conf.h:
